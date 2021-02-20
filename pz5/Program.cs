b75e3f3f6553600c36148fb69f677439c8baf7ad
Шевчугова Вероника Евгеньевна <is12331@stud.KHPK.RU>
2021-02-20 13:21:49 +0700

    pz5v1

---
+++ pz5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        public class ABC
+        {
+            //вывод символов
+            public void letters(string lets)
+            {
+                char[] letters = lets.ToCharArray();
+                foreach (var x in letters)
+                {
+                    Console.WriteLine(x + " ");
+                }
+            }
+        }
+        public partial class ABC
+        {
+            //вызов методы для вывода русских букв
+            public void rusABC()
+            {
+                letters("абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
+            }
+        }
+        public partial class ABC
+        {
+            public void engABC()
+            {
+                //вызов методов для английских букв
+                letters("abcdefghijklmnopqrstuvwxy");
+            }
+        }
+        public class Calc
+        {
+            decimal result;
+            public void Sum(decimal[] arr)
+            {
+                foreach (var x in arr)
+                {
+                    result += x;
+                    Console.WriteLine("Сумма массива: {0}", result);
+                }
+            }
+        }
+        public partial class Calc
+        {
+            public void Mult(decimal[] arr)
+            {
+                decimal y = 1;
+                foreach (var x in arr)
+                {
+                    y *= x;
+                    Console.WriteLine("Сумма массива: {0}", y);
+                }
+                result = y;
+            }
+        }
+
+
+    }
+}
---
+++ pz5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("pz5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pz5")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("751e50e6-10c1-4874-9163-157ef3136916")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
