c2abe2e9c5a0947c18d457382b15536b17c9075f
Шевчугова Вероника Евгеньевна <is12331@stud.KHPK.RU>
2021-03-09 12:09:58 +0700

    work

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pz7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Number n = (Number)(a > 0 ? 1 : (a<0 ? -1 : 0 ));
+            Console.WriteLine("Число {0} {1}", a, n);
+            Console.ReadKey();
+        }
+        public enum Number
+        {
+            отрицательное = -1,
+            нулевое = 0,
+            положительное
+        }
+    }
+}
