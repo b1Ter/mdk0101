a7b7fd4599ccaa1da569b2bfafc271be32017ef1
Шевчугова Вероника Евгеньевна <is12331@stud.KHPK.RU>
2021-03-09 12:34:33 +0700

    work

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pz8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public class DiscountCard : IDiscount
+        {
+            int card;
+            int size;
+            public DiscountCard(int id, int size)
+            {
+                card = id;
+                this.size = size;
+            }
+        }
+        public interface IDiscount
+        {
+            public static int _id;
+            public static int _size;
+            public int DiscountSize
+            {
+                get => _size;
+                set => _size = value;
+            }
+            public int DiscountCard
+            {
+                get => _id;
+                set => _id = value;
+            }
+        }
+    }
+}
